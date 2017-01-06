26955d1aedd8e1cdc4e9ce6aba37840cdb2ebd2e
Jawadban <Jawadbanga@gmail.com>
2017-01-05 17:37:57 -0800

    Finish parse Ajax request Implementing library view.

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,7 +1,24 @@
-// https://api.parse.com/1/classes/songs/
-
 var Songs = Backbone.Collection.extend({
+  initialize: function() {
+    this.connectToServer();    
+  },
 
+  connectToServer: function() {
+    var context = this;
+    Backbone.ajax({
+      url: this.url,
+      method: 'GET',
+      type: 'application/json',
+      success: function(data) {
+        context.add(data.results);          
+        console.log('GET method success');
+      },
+      error: function(song) {
+        console.log('GET method failed');
+      }
+    });
+  },
+  url: 'https://api.parse.com/1/classes/songs/',
   model: SongModel
 
 });
